952ba3b1b834f284114c20f30597201a7ddade66
Jose Cruz <zhonyasgamedev@gmail.com>
2026-04-26 12:06:18 -0600

    feat: a new scene was created (CinemaLoungeScene). Post processing was added to the scene. The neccesary models was added.

---
+++ Assets/TestiTypeWriter.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Febucci.TextAnimatorForUnity;
+using Febucci.TextAnimatorForUnity.TextMeshPro; // <- import Text Animator's namespace
+using TMPro;
+
+public class TestiTypeWriter : MonoBehaviour
+{
+    [SerializeField] private TextAnimator_TMP  textAnimator;
+    [SerializeField] private TypewriterComponent typewriter;
+    // [SerializeField] private string textValue;
+
+    private void Start()
+    {
+         // Do: set text through Text Animator directly
+        textAnimator.SetText("Me he quedado dormido!");
+
+        // textAnimator.AppendText(" world!");
+
+        // typewriter.ShowText(textAnimator.textFull);
+        typewriter.StartShowingText(textAnimator);
+    }
+}
